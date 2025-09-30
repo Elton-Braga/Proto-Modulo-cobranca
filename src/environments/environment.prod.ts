381b91a8a2edcd1b58ae9e9d64d5f11b4b9c0ac1
Elton <elton.b.braga@gmail.com>
2025-09-30 17:05:24 -0300

    ajuste nos ambientes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
-/*export const environment = {
-  production: true,
-  apiUrl: 'https://elton-braga.github.io/Proto-Modulo-cobranca/',
-};*/
-
 export const environment = {
   production: true,
-  apiUrl: 'https://seu-dominio-ou-api.com/api',
+  apiUrl: 'https://elton-braga.github.io/Proto-Modulo-cobranca/',
 };
