8792e247009e388b8c54c197cd78e19500dfa195
Elton <elton.b.braga@gmail.com>
2025-10-05 04:58:54 -0300

    criando página inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './componentes/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    pathMatch: 'full', // garante correspondência exata do caminho vazio
+  },
+  {
+    path: '**', //Rota coringa ('**') → redirecionamento para a Home
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
